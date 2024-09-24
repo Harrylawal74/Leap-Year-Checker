5998d8e4e44fa0e474e3f3661e19518a14cfa35c
Harry Lawal <harrylawal74@gmail.com>
2024-09-24 22:25:31 +0100

    update Main.java
    Now takes year as an input

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
     //takes year as a parameter and runs arithmetic calculations to confirm leap year
@@ -16,7 +18,10 @@ public class Main {
 
     //Test
     public static void main(String[] args) {
-        isLeapYear(800);
+        //Asks user for year as an input and assigns value to 'Year' variable
+        Scanner scanner = new Scanner(System.in);
+        int Year = scanner.nextInt();
+        isLeapYear(Year);
     }
 
 
